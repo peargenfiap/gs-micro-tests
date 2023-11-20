9cf4c6a322f0077086bee486e544f74963c8eb61
peargenfiap <rm88246@fiap.com.br>
2023-11-20 20:10:07 -0300

    feat: removido código desnecessário

--- carga/scripts.js
+++ carga/scripts.js
@@ -1,8 +1,8 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 export const options = {
-  vus: 5000,
-  duration: '180s',
+  vus: 100,
+  duration: '5m',
 };
 export default function () {
   http.get('http://localhost:8080/v1/objetivos/');
