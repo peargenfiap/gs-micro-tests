91c515fc703de4d758dc50aaa96f369b4f545079
peargenfiap <rm88246@fiap.com.br>
2023-11-19 23:38:00 -0300

    test: testes endpoint indicator por id adicionados

--- app.test.js
+++ app.test.js
@@ -70,3 +70,50 @@ describe("GET /v1/objetivos/", () => {
     expect(item.indicadores.length).toBe(4);
   });
 });
+
+describe("GET /v1/indicator/{id}", () => {
+  it("deve ser um array com objetos contendo as propriedades ano e consumo", async () => {
+    const res = await request("http://localhost:8080")
+      .get("/v1/indicator/3.1.1")
+      .expect("Content-Type", /json/)
+      .expect(200);
+
+    expect(res.body).toBeInstanceOf(Array);
+    res.body.forEach((item) => {
+      expect(item).toHaveProperty("ano");
+      expect(item).toHaveProperty("consumo");
+    });
+  });
+
+  it("deve ter ano e consumo como strings", async () => {
+    const res = await request("http://localhost:8080")
+      .get("/v1/indicator/3.1.1")
+      .expect("Content-Type", /json/)
+      .expect(200);
+
+    res.body.forEach((item) => {
+      expect(typeof item.ano).toBe("string");
+      expect(typeof item.consumo).toBe("string");
+    });
+  });
+
+  it("deve ter um objeto específico com ano e consumo corretos", async () => {
+    const res = await request("http://localhost:8080")
+      .get("/v1/indicator/3.1.1")
+      .expect("Content-Type", /json/)
+      .expect(200);
+
+    const item = res.body.find((i) => i.ano === "2015");
+    expect(item).toBeDefined();
+    expect(item.consumo).toBe("62");
+  });
+
+  it("deve ter um número específico de itens", async () => {
+    const res = await request("http://localhost:8080")
+      .get("/v1/indicator/3.1.1")
+      .expect("Content-Type", /json/)
+      .expect(200);
+
+    expect(res.body.length).toBe(10);
+  });
+});
