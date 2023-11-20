702cfdfd45901cd608ec92d6ad6bd85e7d1dcba8
peargenfiap <rm88246@fiap.com.br>
2023-11-20 20:16:50 -0300

    feat: testes de carga implementados

--- app.js
+++ app.js
@@ -7,13 +7,6 @@ const port = 3000;
 app.use(express.json());
 app.get('/', (req, res) => res.json({ message: 'Funcionando!' }));
  
-// GET /aplicarDesconto
-app.get('/aplicarDesconto/:valor/:desconto', (req, res) => {
-    const valor = parseInt(req.params.valor);
-    const desconto = parseInt(req.params.desconto);
-    res.json({ valorDescontado: index.aplicarDesconto(valor, desconto) });
-})
- 
 if (require.main === module) {
     //inicia o servidor
     app.listen(port)
